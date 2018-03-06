be7d71b5918161600375e54c29156dd65f9bfedf
piotrbaranski <p.baranski@yahoo.pl>
2018-03-06 20:18:01 +0100

    Add navbar. Work in progress.

--- public/scripts.js
+++ public/scripts.js
@@ -10,7 +10,45 @@ $("#learn-more-button").click(function () {
   }, 1000);
 });
 
+$('a:not(#burger-nav)').click(function () {
+  $('html, body').animate({
+    scrollTop: $($(this).attr('href')).offset().top
+  }, 1000);
+  return false;
+});
 
 setInterval(function () {
   $('.learn-more').toggleClass('toggle', 1000);
 }, 2000);
+
+function toggleMenu() {
+  var item = document.getElementById('menu');
+  if (item.className === 'hidden') {
+    item.classList.remove('hidden');
+    item.classList.add('show');
+  } else {
+    item.classList.remove('show');
+    item.classList.add('hidden');
+  }
+}
+
+function turnOffHamburger() {
+  var windowWidth = window.innerWidth;
+  var item = document.getElementById('menu');
+  if (windowWidth > 767) {
+    item.classList.remove('hidden', 'show');
+  }
+}
+
+function changeClass() {
+  var hamburger = document.getElementById('burger-nav');
+  hamburger.onclick = toggleMenu;
+}
+
+
+window.onload = function () {
+  changeClass();
+  turnOffHamburger();
+}
+
+window.resize = turnOffHamburger;
