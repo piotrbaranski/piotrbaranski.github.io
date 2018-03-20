03ab6745dead779000a83d0ef3b7286461ed6781
piotrbaranski <p.baranski@yahoo.pl>
2018-03-20 10:10:00 +0100

    Add typed.js. Now hero text is typing when page is loaded.

--- public/scripts.js
+++ public/scripts.js
@@ -1,61 +1,67 @@
 $("#contact-button").click(function () {
-  $('html, body').animate({
-    scrollTop: $("#contact").offset().top
-  }, 1000);
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 1000);
 });
 
 $("#learn-more-button").click(function () {
-  $('html, body').animate({
-    scrollTop: $("#skills").offset().top
-  }, 1000);
+    $('html, body').animate({
+        scrollTop: $("#skills").offset().top
+    }, 1000);
 });
 
 $('a:not(#burger-nav)').click(function () {
-  $('html, body').animate({
-    scrollTop: $($(this).attr('href')).offset().top
-  }, 1000);
-  return false;
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1000);
+    return false;
 });
 
 setInterval(function () {
-  $('.learn-more').toggleClass('toggle', 1000);
+    $('.learn-more').toggleClass('toggle', 1000);
 }, 2000);
 
 function topButtonFunction() {
-  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-    $('#top-button').addClass('active');
-  } else {
-    $('#top-button').removeClass('active');
-  }
+    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+        $('#top-button').addClass('active');
+    } else {
+        $('#top-button').removeClass('active');
+    }
 }
 
 function displayMenu() {
-  var menu = document.getElementById('menu');
-  if (menu.className === 'hidden') {
-    menu.classList.remove('hidden');
-    menu.classList.add('show');
-  } else {
-    menu.classList.remove('show');
-    menu.classList.add('hidden');
-  }
+    var menu = document.getElementById('menu');
+    if (menu.className === 'hidden') {
+        menu.classList.remove('hidden');
+        menu.classList.add('show');
+    } else {
+        menu.classList.remove('show');
+        menu.classList.add('hidden');
+    }
 }
 
 function displayHamburger() {
-  var windowWidth = window.innerWidth;
-  var menu = document.getElementById('menu');
-  windowWidth > 767 ? menu.classList.remove('hidden', 'show') : menu.classList.add('hidden');
+    var windowWidth = window.innerWidth;
+    var menu = document.getElementById('menu');
+    windowWidth > 767 ? menu.classList.remove('hidden', 'show') : menu.classList.add('hidden');
 }
 
 function changeMenuClass() {
-  var hamburger = document.getElementById('burger-nav');
-  hamburger.onclick = displayMenu;
+    var hamburger = document.getElementById('burger-nav');
+    hamburger.onclick = displayMenu;
 }
 
 window.onload = function () {
-  changeMenuClass();
-  displayHamburger();
+    changeMenuClass();
+    displayHamburger();
 }
 
 window.onresize = displayHamburger;
 
 window.onscroll = topButtonFunction;
+
+var typed = new Typed('#typed', {
+    strings: ["Hi, my name is Piotr and I am a junior front-end developer!"],
+    typeSpeed: 70,
+    startDelay: 800
+});
